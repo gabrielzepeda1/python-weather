3d670bff9b46eeb610ea197b16514cf60bca5c32
gabrielzepeda1 <gabrielzepeda846@gmail.com>
2025-04-02 13:57:32 -0600

    adding error handling for connection errors

--- src/main.py
+++ src/main.py
@@ -12,6 +12,9 @@ def main():
     except ValueError as ve:
         print(f"Error: {ve}")
     
+    except ConnectionError as ce: 
+        print(f"Network error: {ce}")
+    
     except Exception as e: 
         print("Something went wrong. Please try again.", e)
 
@@ -19,8 +22,12 @@ def main():
 def get_coordinates(city):
     base_url = "https://geocoding-api.open-meteo.com/v1/search"
     params = {"name": city, "count": 1, "language": "en", "format": "json"}
-    response = requests.get(base_url, params=params)
-    data = response.json()
+
+    try: 
+        response = requests.get(base_url, params=params)
+        data = response.json()
+    except requests.RequestException: 
+        raise ConnectionError("Failed to connect to the geocoding API.")
 
     if data.get("results"): 
         city_data = data["results"][0]
@@ -38,8 +45,12 @@ def get_weather(latitude, longitude):
         "current_weather": True 
     }
 
-    response = requests.get(base_url, params=params)
-    data = response.json()
+    try:
+        response = requests.get(base_url, params=params, timeout=5)
+        data = response.json()
+    except requests.RequestException: 
+        raise ConnectionError("Failed to connect to the weather API.")
+    
     return data["current_weather"]["temperature"]
 
 if __name__ == "__main__":
